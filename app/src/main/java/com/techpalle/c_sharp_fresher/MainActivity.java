e12a1e590ec83eb5bd164db1a2ea2c6777c4ce23
Rishidroid <emailtorishinamdev@gmail.com>
2016-11-15 19:25:52 +0530

    Initial commit

---
+++ app/src/main/java/com/techpalle/c_sharp_fresher/MainActivity.java
@@ -0,0 +1,13 @@
+package com.techpalle.c_sharp_fresher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
